6a3d5df45e909864bdfa52e83c05eb483e7c3c92
kireevr1996 <kireevr1996@gmail.com>
2018-06-01 14:44:30 +0300

    Update 005.py

--- 005.py
+++ 005.py
@@ -1,6 +1,7 @@
 import math as m
 
-def last_prime(n):
+"""""" This function returns the list of primes up to n """""""""
+def prime_list(n):
     numbers = set(range(n, 1, -1))
     primes = []
     while numbers:
@@ -9,10 +10,13 @@ def last_prime(n):
         numbers.difference_update(set(range(p*2, n+1, p)))
     return primes
 
+""""" This function return and prints a number (integer) that can be divided by each nmber that is >= k and give a whole number.
 def least_multiple(k):
+""""" Starting point of multiplication """"""
     N = 1
+""" Limit variable allows us to later decide on which primes need to be expanended (возвести в степень)
     limit = m.sqrt(k)
-    for i in last_prime(k):
+    for i in prime_list(k):
         if i < limit:
             a = int(m.log(k) / m.log(i))
         else:
